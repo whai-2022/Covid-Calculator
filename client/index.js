8ac8c84c880e710c82a188908183d202a28987a0
david-kavenga <david.kavenga@gmail.com>
2022-05-04 18:46:08 +1200

    Add Chakra Provider

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 
@@ -11,9 +12,28 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
 
 document.addEventListener('DOMContentLoaded', () => {
+  const theme = extendTheme({
+    semanticTokens: {
+      colors: {
+        error: 'red.500',
+        success: 'green.500',
+        primary: {
+          default: 'red.500',
+          _dark: 'red.400',
+        },
+        secondary: {
+          default: 'red.800',
+          _dark: 'red.700',
+        },
+      },
+    },
+  })
+  
   render(
     <Provider store={store}>
-      <App />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
     </Provider>,
     document.getElementById('app')
   )
