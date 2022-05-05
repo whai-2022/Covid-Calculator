851eda8d30392a16284143d44c7de42040d956a9
david-kavenga <david.kavenga@gmail.com>
2022-05-05 12:02:26 +1200

    Add Auth0 login
    Co-authored-by: Hannah Victoria Robson <hannah-victoria-robson@users.noreply.github.com>

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Login from './Auth0Login'
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <header className="header">
         <h1>My Collection</h1>
       </header>
+      <Login />
       <section className="main">{/* add your code here */}</section>
     </>
   )
