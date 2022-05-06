debf69b0777b846cf588250f185e31cf5e492125
Hannah Robinson <hannah.robinson.git@gmail.com>
2022-05-07 11:16:20 +1200

    WIP: SuburbResults chain of files

--- client/components/BusinessList.jsx
+++ client/components/BusinessList.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import api from '../apis'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+
+import { getBusinesses } from '../actions'
 import BusinessDetail from './BusinessDetail.jsx'
 
 function BusinessList() {
-  const businesses = api.fetchBusinesses()
+  const dispatch = useDispatch()
+  const { city, suburb } = useParams()
+  const formatLocationName = (locationName) =>
+    locationName.replace(/\s/g, '-').toLowerCase()
+  console.log(formatLocationName(suburb)) // all good, grey-lynn
+
+  const allBusinesses = useSelector((state) => state.businesses)
+  console.log(allBusinesses) // Undefined
+
+  useEffect(() => {
+    dispatch(getBusinesses())
+  }, [])
+
+  const businesses = allBusinesses.filter(
+    (el) =>
+      formatLocationName(el.city) == formatLocationName(city) &&
+      formatLocationName(el.suburb) == formatLocationName(suburb)
+  )
 
   return (
     <>
---
+++ client/reducers/businesses.js
@@ -0,0 +1,11 @@
+import { RECEIVE_BUSINESSES } from '../actions'
+
+const initialState = {}
+export default function businesses(state = initialState, action) {
+  switch (action.type) {
+    case RECEIVE_BUSINESSES:
+      return action.payload
+    default:
+      return state
+  }
+}
