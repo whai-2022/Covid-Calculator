4a67f7bdfdd57f8f1c5014debf7835a13c549b3a
david-kavenga <david.kavenga@gmail.com>
2022-05-08 11:05:30 +1200

    Add body box to App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,30 +1,53 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { Container, Box } from '@chakra-ui/react'
 
 import Navbar from './Navbar'
+import HomeSplash from './HomeSplash'
 import BusinessListingPage from './BusinessListingPage'
+import CitiesSuburbs from './CitiesSuburbs'
 import SearchPage from './SearchPage'
-import MapContainer from './MapContainer'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={'home'}></Route>
-        <Route
-          exact
-          path="businesses/:id"
-          element={<BusinessListingPage />}
-        ></Route>
-        <Route exact path="cities" element={'cities'}></Route>
-        <Route exact path="cities/:city" element={'city'}></Route>
-        <Route
-          exact
-          path="cities/:city/:suburb"
-          element={<SearchPage />}
-        ></Route>
-      </Routes>
+      <Container
+        w="100%"
+        maxW="100%"
+        minH="100vh"
+        bg="blue.100"
+        p={0}
+        display="flex"
+        flexDirection="column"
+        align="flex-start"
+      >
+        <Navbar />
+        <Container w="100%" maxW="100%" flexGrow="1" centerContent>
+          <Box
+            width={{ base: 'full', md: '80%' }}
+            maxW="1600px"
+            flexGrow="1"
+            bg="white"
+            p={5}
+          >
+            <Routes>
+              <Route exact path="/" element={<HomeSplash />}></Route>
+              <Route
+                exact
+                path="businesses/:id"
+                element={<BusinessListingPage />}
+              ></Route>
+              <Route exact path="cities" element={<CitiesSuburbs />}></Route>
+              <Route exact path="cities/:city" element={'city'}></Route>
+              <Route
+                exact
+                path="cities/:city/:suburb"
+                element={<SearchPage />}
+              ></Route>
+            </Routes>
+          </Box>
+        </Container>
+      </Container>
     </>
   )
 }
