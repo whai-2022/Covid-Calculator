27b51a202b3324d4d76614a1d3d4b88c527d3a69
david-kavenga <david.kavenga@gmail.com>
2022-05-08 11:05:05 +1200

    Add Component Skeletons

---
+++ client/components/HomeSplash.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Heading } from '@chakra-ui/react'
+
+function HomeSplash() {
+  return (
+    <>
+      <Heading as="h1" size="lg" letterSpacing={'tighter'}>
+        Home Splash
+      </Heading>
+    </>
+  )
+}
+
+export default HomeSplash
