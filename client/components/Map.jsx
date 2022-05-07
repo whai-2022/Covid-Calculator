6aa8f14ad21c24ae14318cef601b9056822cc669
Hannah Robinson <hannah.robinson.git@gmail.com>
2022-05-07 13:35:18 +1200

    Add placeholder Map component

---
+++ client/components/Map.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+function Map() {
+  return <div>I am a map</div>
+}
+
+export default Map
--- client/components/SuburbResults.jsx
+++ client/components/SuburbResults.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import BusinessList from './BusinessList'
+import Map from './Map'
 
 function SuburbResults() {
   return (
     <>
-      {/* <Map /> */}
+      <Map />
       <BusinessList />
     </>
   )
