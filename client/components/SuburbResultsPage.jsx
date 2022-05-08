7602dc34c36e170dbb8d0e5d91b0c7a6766ab40e
david-kavenga <david.kavenga@gmail.com>
2022-05-08 21:34:14 +1200

    Add dynamic suburbs on Map

--- client/components/SuburbResultsPage.jsx
+++ client/components/SuburbResultsPage.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Heading } from '@chakra-ui/react'
 
 import BusinessList from './BusinessList'
 import MapContainer from './MapContainer'
 
+import api from '../apis'
+
 function SuburbResultsPage() {
   const { city, suburb } = useParams()
+  const [location, setLocation] = useState(null)
+
   const formatLocationName = (locationName) => {
     return locationName
       ?.replace(/-/g, ' ')
@@ -17,12 +21,25 @@ function SuburbResultsPage() {
       .join(' ')
   }
 
+  useEffect(() => {
+    api
+      .fetchCoordinates(
+        `${formatLocationName(suburb)}, ${formatLocationName(city)}`
+      )
+      .then((res) => {
+        setLocation(res.features[0].center)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }, [])
+
   return (
     <>
       <Heading as="h1" size="lg" letterSpacing={'tighter'}>
         Listings for {formatLocationName(suburb)}, {formatLocationName(city)}
       </Heading>
-      <MapContainer />
+      {location && <MapContainer location={location} />}
       <BusinessList />
     </>
   )
