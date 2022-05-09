acb7ed2b5c4379896175388385a4b5404f2233aa
Hannah Robinson <hannah.robinson.git@gmail.com>
2022-05-09 18:15:58 +1200

    WIP: reliability score component

---
+++ client/components/ReliabilityScore.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+function ReliabilityScore() {
+  const allBusinesses = useSelector((state) => state.businesses)
+}
+
+export default ReliabilityScore
