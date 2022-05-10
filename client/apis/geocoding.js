e13aad8e77b4a7073d269ac6718330fbad5e7fe2
david-kavenga <david.kavenga@gmail.com>
2022-05-11 09:46:01 +1200

    Remove console logs

--- client/apis/geocoding.js
+++ client/apis/geocoding.js
@@ -4,7 +4,6 @@ const access_token = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
 
 export function fetchCoordinates(location) {
   const locationEncoded = encodeURIComponent(location)
-  console.log(locationEncoded)
   return request
     .get(
       `https://api.mapbox.com/geocoding/v5/mapbox.places/${locationEncoded}.json?country=nz&access_token=${access_token}`
