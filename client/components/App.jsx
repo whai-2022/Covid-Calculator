0569432bc297faa134b94b29bdc192805e657e47
Hannah-Robson <hannah.v.robson@gmail.com>
2022-05-10 23:18:21 +1200

    add breadcrumbs

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,7 @@ import { Container, Box } from '@chakra-ui/react'
 
 import Navbar from './Navbar'
 import HomeSplash from './HomeSplash'
+import Breadcrumbs from './Breadcrumbs'
 import BusinessListingPage from './BusinessListingPage'
 import SuburbResultsPage from './SuburbResultsPage'
 import CitiesSuburbs from './CitiesSuburbs'
@@ -31,6 +32,7 @@ function App() {
             bg="white"
             p={5}
           >
+            <Breadcrumbs key={window.location.pathname} />
             <Routes>
               <Route exact path="/" element={<HomeSplash />}></Route>
               <Route
---
+++ client/components/Breadcrumbs.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Text,
+} from '@chakra-ui/react'
+import { Link, useLocation } from 'react-router-dom'
+import { BiChevronRight } from 'react-icons/bi'
+
+function Breadcrumbs() {
+  const location = useLocation()
+  const { pathname } = location
+  const splitLocation = pathname.split('/')
+
+  const formatLocationName = (locationName) => {
+    return locationName
+      ?.replace(/-/g, ' ')
+      .split(' ')
+      .map(
+        (word) => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
+      )
+      .join(' ')
+  }
+
+  console.log(pathname)
+  return (
+    <>
+      <Breadcrumb spacing="3px" separator={<BiChevronRight color="gray.500" />}>
+        {pathname == '/'
+          ? ''
+          : splitLocation.map((location, i) => {
+              return (
+                <BreadcrumbItem key={location + i}>
+                  <BreadcrumbLink
+                    as={Link}
+                    to={splitLocation.slice(0, i + 1).join('/')}
+                  >
+                    <Text>
+                      {location == '' ? 'Home' : formatLocationName(location)}
+                    </Text>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              )
+            })}
+      </Breadcrumb>
+    </>
+  )
+}
+
+export default Breadcrumbs
