85f8de9502e1010d4bc4af98b16d939a842a63dc
david-kavenga <david.kavenga@gmail.com>
2022-05-11 13:25:14 +1200

    Remove commented code

--- client/components/Auth0Login.jsx
+++ client/components/Auth0Login.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { Button, Image, Stack, Text, HStack } from '@chakra-ui/react'
-// import { Link } from 'react-router-dom'
 
 const Login = () => {
   const { isAuthenticated, user, logout, loginWithRedirect } = useAuth0()
